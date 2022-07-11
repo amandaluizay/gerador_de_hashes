837a5f78f473979b7672edb66f7bb47a0aeb9419
amanda <luiza.amandasilvah@gmail.com>
2022-07-11 10:11:39 -0300

    first commit

---
+++ main.py
@@ -0,0 +1,25 @@
+import hashlib
+
+string = input("Digite o texto a ser criptografado: ")
+
+menu = int(input('''MENU - ESCOLHA O TIPO DE HASH
+                 1) MD5
+                 2) SHA1
+                 3) SHA256
+                 4) SHA512
+                 Insira o numero: '''))
+
+if menu == 1:
+    resultado = hashlib.md5(string.encode('utf-8'))
+    h = 'MD5'
+elif menu == 2:
+    resultado = hashlib.sha1(string.encode('utf-8'))
+    h = 'SHA1'
+elif menu == 3:
+    resultado = hashlib.sha256(string.encode('utf-8'))
+    h = 'SHA256'
+elif menu == 4:
+    resultado = hashlib.sha512(string.encode('utf-8'))
+    h = 'SHA12'
+
+print("O hash", h, "da string", string, "é:", resultado.hexdigest())
